1ba49f20c3333db1c3b594883356fd15b9fd47c4
Yevhenii Deina <deinak1996@gmail.com>
2023-12-16 14:07:43 +0200

    storybook components

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -2,11 +2,10 @@ import { addDecorator } from '@storybook/react';
 
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 
-import 'app/styles/index.scss';
-
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 
 import { Theme } from '../../src/app/providers/ThemeProvider';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
 
 export const parameters = {
 
@@ -29,3 +28,5 @@ export const parameters = {
 addDecorator(StyleDecorator);
 
 addDecorator(ThemeDecorator(Theme.LIGHT));
+
+addDecorator(RouterDecorator);
